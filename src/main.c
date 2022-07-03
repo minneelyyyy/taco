15e1328e8a8c41187e585448052fbdba81cf8fad
April Kahny <aprilkahny@gmail.com>
2022-07-03 16:21:16 -0400

    add basic repl, insecure and buggy

--- src/main.c
+++ src/main.c
@@ -5,23 +5,36 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+#define COL_RED     "\x1b[1;31m"
+#define COL_RESET   "\x1b[0m"
+
 int main(int argc, char **argv, char **envp) {
     char buffer[1024] = { 0 };
     int proc;
     siginfo_t child_info;
 
-    proc = fork();
+    for (;;) {
+        printf("$ ");
+        fgets(buffer, 1024, stdin);
 
-    if (proc == 0) {
-        /* TODO: use exec */
-        printf("Hello, World!\n");
-        exit(69);
+        /* handle shell specific commands */
+        if (strcmp(buffer, "exit\n") == 0) {
+            break;
+        }
+
+        proc = fork();
+
+        if (proc == 0) {
+            /* system() relies on sh being the shell, this means
+             * taco does not yet work as a stand-alone shell */
+            exit(system(buffer));
+        }
+
+        waitid(P_PID, proc, &child_info, WEXITED | WSTOPPED);
+
+        if (child_info.si_status != 0)
+            printf(COL_RED "status %d" COL_RESET "\n", child_info.si_status);
     }
-
-    waitid(P_PID, proc, &child_info, WEXITED | WSTOPPED);
-
-    if (child_info.si_status != 0)
-        printf("\x1b[1;31mstatus %d\x1b[0m\n", child_info.si_status);
 
     return 0;
 }
